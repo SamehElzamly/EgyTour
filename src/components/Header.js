6f2e03e70c5d86f4e487213638843815978bcf7b
sameh <sameh231115@gmail.com>
2023-02-10 20:54:08 +0200

    end of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot ,faCalendarDays,faUsers,faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
 export function Header(){
     return(
         <>    
         <div></div>    
         <span>
+            <div ></div>
+            <div ></div>
             <div ></div>
             <div ></div>
             <div ></div>
@@ -19,6 +23,31 @@ export function Header(){
                 <button>Sign Up</button>
             </div>
         </header>
+        <div className="headerInfo">
+            <h1>Discover <span>self-guided </span>itineraries</h1>
+            <p>More than 20 trip in sharm are ready for you.</p>
+        </div>
+        <div className="navbar">
+            <ul>
+                <li>
+                <div><FontAwesomeIcon icon={faLocationDot} /><p>Trip</p></div>
+                <p>Choose a trip</p>
+                </li>
+                <li>
+                <div><FontAwesomeIcon icon={faCalendarDays} /><p>Dates</p></div>
+                <p>Choose a date</p>
+                </li>
+                <li>
+                <div><FontAwesomeIcon icon={faUsers} /><p>Travelers</p></div>
+                <p>Number of tickets</p>
+                </li>
+               
+                <li>
+                     <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </li>
+            </ul>
+           
+        </div>
         </>
     );
 }
